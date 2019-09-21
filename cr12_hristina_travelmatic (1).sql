8a6f2e5a68582f53fd9e997d35a9ee0338ac08ba
Hristina Dakic <hristina.dakic@gmail.com>
2019-09-21 20:16:49 +0200

    adds new data

--- cr12_hristina_travelmatic (1).sql
+++ cr12_hristina_travelmatic (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 21, 2019 at 04:54 PM
+-- Generation Time: Sep 21, 2019 at 08:01 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -71,8 +71,8 @@ CREATE TABLE `restaurants` (
 --
 
 INSERT INTO `restaurants` (`restaurantId`, `restaurantName`, `restaurantImage`, `restaurantType`, `restaurantDescription`, `restaurantTel`, `restaurantWeb`, `restaurantAddress`) VALUES
-(2, 'ALL REIS', 'https://www.mobilitylounge.com/reisen/wp-content/uploads/2018/12/allreis4.jpg', '#Asian #Thai', 'All Reis is a food sensation in the 15th district, that is already a favourite for those in the know in Vienna.', '01 7864668', 'https://www.allreis.com/', 'Schweglerstraße 12, 1150 Vienna'),
-(3, 'Café Ansari', 'https://lokalfuehrer.stadtbekannt.at/wp-content/uploads/2016/08/Cafe-Ansari-Lokal-Bar-Tische-c-STADTBEKANNT.jpg', '#Georgian', 'The \'Café Ansari\' offers a combination of Mediterranean, Georgian and international cuisine', '01 7864668', 'https://cafeansari.at/', 'Praterstraße 15, 1020 Vienna'),
+(2, 'ALL REIS', 'https://www.mobilitylounge.com/reisen/wp-content/uploads/2018/12/allreis4.jpg', '#Asian #Thai', 'All Reis is a food sensation in the 15th district, that is already a favourite for those in the know in Vienna.', '01 7864668', 'https://www.allreis.com/', 'SchweglerstraÃŸe 12, 1150 Vienna'),
+(3, 'CafÃ© Ansari', 'https://lokalfuehrer.stadtbekannt.at/wp-content/uploads/2016/08/Cafe-Ansari-Lokal-Bar-Tische-c-STADTBEKANNT.jpg', '#Georgian', 'CafÃ© Ansari offers a combination of Mediterranean, Georgian and international cuisine', '01 7864668', 'https://cafeansari.at/', 'PraterstraÃŸe 15, 1020 Vienna'),
 (4, 'Plachutta', 'https://1.bp.blogspot.com/-8gX6dj_YNRg/WHd092VcmyI/AAAAAAAAvJA/uX52iC1lcWAddOh28uY3Q6g1DztjLtPCwCLcB/s1600/IMG_7887.JPG', '#Austrian', 'Smart venue for Tafelspitz boiled beef & other classics of Viennese cuisine.', '01 5121577', 'https://www.plachutta-wollzeile.at/', 'Wollzeile 38, 1010 Vienna'),
 (5, 'Restaurant Mill', 'http://www.mill32.at/typo3temp/pics/fdf2118d8f.jpg', '#Austrian #European', 'Eclectic contemporary dining room with outside seating, candlelit tables & a modern global menu.', '01 9664073', 'http://www.mill32.at/', 'Millergasse 32, 1060 Vienna'),
 (9, 'Das Augustin', 'https://1000things.b-cdn.net/wp-content/uploads/augustin-fb-teaser.jpg', '#European', 'Comfortable environment and delicious meals. If you want to enjoy the breakfast time, please do not miss this place.', '01 9821364', 'http://www.das-augustin.at/', 'MÃ¤rzstraÃŸe 67, 1150 Wien');
@@ -98,10 +98,10 @@ CREATE TABLE `sigtseeingPlaces` (
 --
 
 INSERT INTO `sigtseeingPlaces` (`sightseeingId`, `sightseeingName`, `sightseeingImage`, `sightseeingType`, `sightseeingDescription`, `sightseeingWeb`, `sightseeingAddress`) VALUES
-(1, 'St. Charles Church', 'http://www.karlskirche.at/images/karlskirche.jpg', '#church', 'bla bla decription', 'https://www.wien.info/en/sightseeing/sights/from-g-to-k/st-charles-church', 'Karlsplatz 1, 1010 Vienna'),
-(2, 'KunstHausWien', 'https://www.hundertwasser-kalender.de/WebRoot/Store22/Shops/62420907/54ED/E3E8/3FA7/6338/5C97/C0A8/2BB9/B55F/AK-029_KunstHausWien_01_ml.jpg', '#museum #gallery', 'bla bla decription', 'https://www.kunsthauswien.com/en/', 'Untere Weißgerberstraße 13, 1030 Vienna'),
-(3, 'Stadtpark City Park', 'https://www.wien.gv.at/umwelt/parks/anlagen/images/stadtpark.jpg', '#park', 'bla bla decription', 'https://www.wien.gv.at/english/environment/parks/stadtpark.html ', 'Parkring 1, 1010 Vienna'),
-(4, 'Schönbrunn Palace', 'https://www.schoenbrunn.at/fileadmin/_processed_/d/7/csm_Ehrenhof_Brunnen_Ost__4__e01ccf3caf.jpg', '#palace #museum #park', 'bla bla decription', 'https://www.schoenbrunn.at/en/', 'Schönbrunner Schloßstraße 47, 1130 Vienna'),
+(1, 'St. Charles Church', 'http://www.karlskirche.at/images/karlskirche.jpg', '#church', 'Widely considered the most outstanding baroque church in Vienna, as well as one of the city&#39;s greatest buildings.', 'https://www.wien.info/en/sightseeing/sights/from-g-to-k/st-charles-church', 'Karlsplatz 1, 1010 Vienna'),
+(2, 'KunstHausWien', 'https://www.hundertwasser-kalender.de/WebRoot/Store22/Shops/62420907/54ED/E3E8/3FA7/6338/5C97/C0A8/2BB9/B55F/AK-029_KunstHausWien_01_ml.jpg', '#museum #gallery', 'The KunstHausWien is a museum in Vienna, designed by the artist Friedensreich Hundertwasser. ', 'https://www.kunsthauswien.com/en/', 'Untere WeiÃŸgerberstraÃŸe 13, 1030 Vienna'),
+(3, 'Stadtpark City Park', 'https://www.wien.gv.at/umwelt/parks/anlagen/images/stadtpark.jpg', '#park', 'The City Park in Vienna meets every expectation of an \"artificial landscape in the city\". ', 'https://www.wien.gv.at/english/environment/parks/stadtpark.html', 'Parkring 1, 1010 Vienna'),
+(4, 'SchÃ¶nbrunn Palace', 'https://www.schoenbrunn.at/fileadmin/_processed_/d/7/csm_Ehrenhof_Brunnen_Ost__4__e01ccf3caf.jpg', '#palace #museum #park', 'The Palace was the main summer residence of the Habsburg rulers. It is one of the most important historic monuments in the country. ', 'https://www.schoenbrunn.at/en/', 'SchÃ¶nbrunner SchloÃŸstraÃŸe 47, 1130 Vienna'),
 (5, 'MuseumsQuartier', 'https://www.leopoldmuseum.org/media/image/c950x576/360.jpg', '#museum', 'The MuseumsQuartier (MQ) is one of the largest cultural quarters in the world. ', 'https://www.mqw.at/en/', 'Museumsplatz 1, 1070 Wien');
 
 -- --------------------------------------------------------
